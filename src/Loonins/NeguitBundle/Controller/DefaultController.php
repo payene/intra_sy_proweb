8723845a6e82ca960ebb7958beedc1866556a76d
unknown <denis.kombate@gmail.com>
2019-03-26 04:24:29 +0000

    Add. puis commit : Creation du bundle neguit

---
+++ src/Loonins/NeguitBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Loonins\NeguitBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('LooninsNeguitBundle:Default:index.html.twig');
+    }
+}
---
+++ src/Loonins/NeguitBundle/LooninsNeguitBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Loonins\NeguitBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class LooninsNeguitBundle extends Bundle
+{
+}
